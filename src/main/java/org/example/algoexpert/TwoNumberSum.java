c06f2b3b460c21d4eedab95c48b110a4797717f6
Joao Zarate <joaozarate@gmail.com>
2023-04-19 11:30:54 +0100

    Adds Two Number Sum algo.

---
+++ src/main/java/org/example/algoexpert/TwoNumberSum.java
@@ -0,0 +1,31 @@
+package org.example.algoexpert;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoNumberSum {
+
+    public static void main(String[] args) {
+
+        twoNumberSum(new int[] {3, 5, -4, 8, 11, 1, -1, 6}, 10);
+
+    }
+
+    static int[] twoNumberSum(int[] array, int target) {
+
+        Set<Integer> nums = new HashSet<>();
+
+        for (int num : array) {
+            int potentialMatch = target - num;
+            /* The method contains is not an under-the-hood 'for' statement, but a 'map' */
+            if (nums.contains(potentialMatch)) {
+                return new int[] {potentialMatch, num};
+            } else {
+                nums.add(num);
+            }
+        }
+
+        return new int[0];
+    }
+
+}
