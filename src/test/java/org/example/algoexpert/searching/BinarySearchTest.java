c8e592c23cea0f1e0b56e10021310804ee16af94
Joao Zarate <joaozarate@gmail.com>
2023-04-20 11:54:58 +0100

    Adds binary search algo.

---
+++ src/test/java/org/example/algoexpert/searching/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package org.example.algoexpert.searching;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTest {
+
+    @Test
+    void Test() {
+        int position = BinarySearch.binarySearch(new int[] {0, 1, 21, 33, 45, 45, 61, 71, 72, 73}, 33);
+        Assertions.assertEquals(3, position);
+    }
+
+}
