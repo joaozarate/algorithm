38b3cdfebbbdb98dec0b710a098b6298aa411669
Joao Zarate <joaozarate@gmail.com>
2023-04-21 19:08:46 +0100

    Adds sorting  algo.

---
+++ src/test/java/org/example/algoexpert/sorting/BubbleSortTest.java
@@ -0,0 +1,29 @@
+package org.example.algoexpert.sorting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    public void TestCase1() {
+        int[] expected = {2, 3, 5, 5, 6, 8, 9};
+        int[] input = {8, 5, 2, 9, 5, 6, 3};
+        Assertions.assertTrue(compare(BubbleSort.execute(input), expected));
+    }
+
+    private boolean compare(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
