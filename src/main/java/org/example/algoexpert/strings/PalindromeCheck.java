4fd4f00e8d72c4f34136a31657dadc86cac9f4bd
Joao Zarate <joaozarate@gmail.com>
2023-04-21 19:26:11 +0100

    Adds palindrome check algo.

---
+++ src/main/java/org/example/algoexpert/strings/PalindromeCheck.java
@@ -0,0 +1,23 @@
+package org.example.algoexpert.strings;
+
+public class PalindromeCheck {
+
+    static boolean execute(String str) {
+
+        String string = str.toLowerCase().replace(" ", "");
+
+        int leftIdx = 0;
+        int rightIdx = string.length() - 1;
+
+        while (leftIdx < rightIdx) {
+            if (string.charAt(leftIdx) != string.charAt(rightIdx)) {
+                return false;
+            }
+            leftIdx += 1;
+            rightIdx -= 1;
+        }
+
+        return true;
+    }
+
+}
