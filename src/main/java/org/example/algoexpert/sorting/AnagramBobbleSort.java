6e4be2a50a66f25cdfbca229e8865be4ca32300e
Joao Zarate <joaozarate@gmail.com>
2023-04-22 23:07:12 +0100

    Adds anagram algo.

---
+++ src/main/java/org/example/algoexpert/sorting/AnagramBobbleSort.java
@@ -0,0 +1,46 @@
+package org.example.algoexpert.sorting;
+
+public class AnagramBobbleSort {
+
+    static boolean execute(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] char1 = s1.toCharArray();
+        char[] char2 = s2.toCharArray();
+
+        boolean isSorted = false;
+        int counter = 0;
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < char1.length - 1 - counter; i++) {
+                if (char1[i] > char1[i + 1]) {
+                    swap(i, i + 1, char1);
+                    isSorted = false;
+                }
+                if (char2[i] > char2[i + 1]) {
+                    swap(i, i + 1, char2);
+                    isSorted = false;
+                }
+            }
+            char lastChar1 = char1[char1.length - 1 - counter];
+            char lastChar2 = char2[char2.length - 1 - counter];
+            if (lastChar1 != lastChar2) {
+                return false;
+            }
+            counter += 1;
+        }
+
+        return true;
+    }
+
+    private static void swap(int i, int j, char[] arrayChar) {
+        char aux = arrayChar[j];
+        arrayChar[j] = arrayChar[i];
+        arrayChar[i] = aux;
+    }
+
+}
--- src/test/java/org/example/algoexpert/sorting/BubbleSortTest.java
+++ src/test/java/org/example/algoexpert/sorting/BubbleSortTest.java
@@ -3,8 +3,6 @@ package org.example.algoexpert.sorting;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BubbleSortTest {
 
     @Test
